b240fcc53b74b4e81c462846a40f8766c74fec58
montuckymike <mike@kosmojo.com>
2017-06-30 11:07:39 -0600

    updating js file with constructor functions

--- miniWorkshops/birdsVue/js/app.js
+++ miniWorkshops/birdsVue/js/app.js
@@ -4,4 +4,22 @@ var b1 = {
   species: "Meadowlark",
   img: ""
 };
+var b2 = {
+  name: "Pygmy Owl",
+  gender: "Female",
+  species: "Owl",
+  img: ""
+};
 console.log(b1);
+console.log(b2);
+
+function Bird (name, species, gender, img){
+  this.name = name;
+  this.species = species;
+  this.gender = gender;
+  this.img = img;
+}
+
+var b3 = new Bird("brown headed cowbird", "cowbird", "male", "https://www.allaboutbirds.org/guide/PHOTO/LARGE/brown_headed_cowbird_4.jpg");
+
+console.log(b3);
